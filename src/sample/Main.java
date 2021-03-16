1ae355fbe677f8bcdd245cef5b15b32c4e5c8707
subhanmangi <subhanmangi@gmail.com>
2021-03-16 13:08:09 +0500

    GUI Done

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Tour-Planner");
+
+
+
+        primaryStage.setScene(new Scene(root, 462, 325));
         primaryStage.show();
     }
 
